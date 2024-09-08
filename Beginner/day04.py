9466d2d99be6e504f61c04a5264afa1f32b5dfde
straystringz <victor.j.emmanuel@hotmail.com>
2024-09-08 23:01:28 +0100

    I made my Treasure map implementation

--- Beginner/day04.py
+++ Beginner/day04.py
@@ -12,8 +12,31 @@ import random
 
 # Banker Roulette
 
-names = input("Please enter your names separated bu a ',' and a space:\n")
-name_list = names.split(", ")
+# names = input("Please enter your names separated bu a ',' and a space:\n")
+# name_list = names.split(", ")
 
-index = random.randint(0, len(name_list - 1))
-print(f"{name_list[index]} is going to pay for today's meal")
+# index = random.randint(0, len(name_list - 1))
+# print(f"{name_list[index]} is going to pay for today's meal")
+
+# ____________________________________________________________
+
+
+# Treasure Map
+
+r1 = ["🔲", "🔲", "🔲"]
+r2 = ["🔲", "🔲", "🔲"]
+r3 = ["🔲", "🔲", "🔲"]
+
+map = [r1, r2, r3]
+print(f"{r1}\n{r2}\n{r3}\n")
+
+position = input("Where do you want to put the treasure?\n")
+column = int(position[0]) - 1
+row = int(position[1]) - 1
+
+# map[column] = "X"
+# map[row] = "X"
+
+map[row][column] = "X"
+
+print(f"{r1}\n{r2}\n{r3}\n")
